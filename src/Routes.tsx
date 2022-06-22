d4888dccb9773652595016b83e080d8ddfde672b
Thais Cavalcante <thaispcavalcante@gmail.com>
2022-06-22 18:04:57 -0300

    acrescentando novas rotas (Produtos, Entregas e Fornecedores)

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,12 +1,17 @@
 import {Routes as WrapperRoutes, Route, BrowserRouter} from 'react-router-dom';
 
-import User from './pages/User';
+import User from './pages/Users';
+import Entregas from './pages/Entregas';
+import Fornecedores from './pages/Fornecedores';
 
 function Routes(): JSX.Element {
     return (
         <BrowserRouter>
         <WrapperRoutes>
             <Route path='/' element={<User />}></Route>
+            <Route path='/entregas' element={<Entregas />}></Route>
+            <Route path='/fornecedores' element={<Fornecedores />}></Route>
+            {/* <Route path='/produtos' element={<Produtos />}></Route> */}
         </WrapperRoutes>
         </BrowserRouter>
     )
