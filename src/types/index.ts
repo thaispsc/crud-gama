f76da314c74290c9ac100abbc646f2344f3aa67b
Thais Cavalcante <thaispcavalcante@gmail.com>
2022-06-22 18:03:32 -0300

    adicionando types Produtos, Entregas e Fornecedores

--- src/types/index.ts
+++ src/types/index.ts
@@ -7,3 +7,29 @@ export interface User {
     email: string,
     telefone: string
 }
+
+export interface Fornecedor {
+    id: number,
+    razao: string,
+    cnpj: number,
+    endereco: string,
+    email: string,
+    telefone: string
+}
+
+export interface Produto {
+    id: number,
+    nome: string,
+    descricao: string,
+    preco: number,
+    quantidade: number
+}
+
+export interface Entrega {
+    id: number,
+    nome: string,
+    cnpj: number,
+    endereco: string,
+    email: string,
+    telefone: string
+}
