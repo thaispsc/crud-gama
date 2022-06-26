69b86d823d0aacfd4cbf4937ce19d7d4ff50a6f8
Thais Cavalcante <thaispcavalcante@gmail.com>
2022-06-25 23:44:52 -0300

    hooks modal Entregas

--- src/hooks/Entregas/useModal.ts
+++ src/hooks/Entregas/useModal.ts
@@ -1,1 +1,39 @@
-0 
+import {useState} from 'react';
+import {Entrega} from '../../types';
+
+const useModal = () => {
+  const [entregaCreateModal, setEntregaCreateModal] = useState<boolean>(false);
+  const [entregaEditModal, setEntregaEditModal] = useState<Entrega>({} as Entrega);
+
+  // Função para abrir o nosso modal
+  const handleOpenCreateEntregaModal = () => {
+    setEntregaCreateModal(true)
+  }
+
+  // Função para fechar o nosso modal
+  const handleCloseCreateEntregaModal = () => {
+    setEntregaCreateModal(false)
+  }
+
+    // Função para abrir o nosso modal de edição
+    const handleOpenEditEntregaModal = (entrega: Entrega) => {
+      setEntregaEditModal(entrega)
+    }
+
+     // Função para fechar o nosso modal de edição
+  const handleCloseEditEntregaModal = () => {
+    setEntregaEditModal({} as Entrega)
+  }
+
+
+  return {
+    entregaCreateModal,
+    handleOpenCreateEntregaModal,
+    handleCloseCreateEntregaModal, 
+    entregaEditModal,
+    handleOpenEditEntregaModal,
+    handleCloseEditEntregaModal
+  };
+};
+
+export default useModal;
