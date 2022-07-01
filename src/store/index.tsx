bbfd8bad23354fea14351ac64209685751fce875
Thais Cavalcante <thaispcavalcante@gmail.com>
2022-06-30 23:17:02 -0300

    Store

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,9 +1,1 @@
 import { configureStore } from '@reduxjs/toolkit';
-import usersSlice from './users';
-
-export const store = configureStore({
-	reducer: {
-		usersSlice,
-    // persistedReducer,
-	},
-});
